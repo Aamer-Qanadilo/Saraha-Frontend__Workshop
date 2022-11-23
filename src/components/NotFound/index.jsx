96137f8d8ab0c25f058b8e8f3dd839769a5cfc59
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2022-11-23 22:45:46 +0200

    Added few features and fixed some problems

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.notFound}>
+      <p>Page wasn't found</p>
+    </div>
+  );
 };
 
 export default NotFound;
--- src/components/UserContext/UserProvider.jsx
+++ src/components/UserContext/UserProvider.jsx
@@ -1,10 +1,19 @@
-import React, { createContext, useState } from "react";
+import jwtDecode from "jwt-decode";
+import React, { createContext, useEffect, useState } from "react";
 import cookie from "react-cookies";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [loggedUser, setLoggedUser] = useState(cookie.load("token"));
+  const [loggedUser, setLoggedUser] = useState("");
+
+  useEffect(() => {
+    const token = cookie.load("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      setLoggedUser(decoded);
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ loggedUser, setLoggedUser }}>
